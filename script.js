42ce1284f21e3fa307d9e6618f44e8fe3c750f9b
Samana Gyawali <shreyagyawali969@gmail.com>
2026-05-26 14:05:20 +0545

    Added local storage functionality

--- script.js
+++ script.js
@@ -1,38 +1,67 @@
+let students = JSON.parse(localStorage.getItem("students")) || [];
+
+window.onload = function () {
+    displayStudents();
+};
+
 function addStudent() {
-     let ID= document.getElementById("ID").value;
+    let ID = document.getElementById("ID").value;
     let name = document.getElementById("name").value;
     let age = document.getElementById("age").value;
     let course = document.getElementById("course").value;
 
-    if(ID ===""|| name === "" || age === "" || course === "") {
+    if (ID === "" || name === "" || age === "" || course === "") {
         alert("Please fill all fields");
         return;
- }
-     if(age <= 0) {
-   alert("Invalid age");
-         return;
-     }
+    }
 
-    let table = document.getElementById("studentList");
+    if (age <= 0) {
+        alert("Invalid age");
+        return;
+    }
 
-    let row = table.insertRow();
-    
-    row.insertCell(0).innerHTML = ID;
-    row.insertCell(1).innerHTML = name;
-    row.insertCell(2).innerHTML = age;
-    row.insertCell(3).innerHTML = course;
+    let student = {
+        ID: ID,
+        name: name,
+        age: age,
+        course: course
+    };
 
-    let actionCell = row.insertCell(4);
+    students.push(student);
 
-    actionCell.innerHTML = 
-    `<button class="delete-btn" onclick="deleteStudent(this)">Delete</button>`;
-    document.getElementById("ID").value ="";
-    document.getElementById("name").value ="";
-    document.getElementById("age").value ="";
-    document.getElementById("course").value ="";
+    localStorage.setItem("students", JSON.stringify(students));
+
+    displayStudents();
+
+    document.getElementById("ID").value = "";
+    document.getElementById("name").value = "";
+    document.getElementById("age").value = "";
+    document.getElementById("course").value = "";
 }
 
-function deleteStudent(button) {
-    let row = button.parentElement.parentElement;
-    row.remove();
+function displayStudents() {
+    let table = document.getElementById("studentList");
+    table.innerHTML = "";
+
+    students.forEach((student) => {
+        let row = table.insertRow();
+
+        row.insertCell(0).innerHTML = student.ID;
+        row.insertCell(1).innerHTML = student.name;
+        row.insertCell(2).innerHTML = student.age;
+        row.insertCell(3).innerHTML = student.course;
+
+        let actionCell = row.insertCell(4);
+
+        actionCell.innerHTML =
+            `<button onclick="deleteStudent('${student.ID}')">Delete</button>`;
+    });
+}
+
+function deleteStudent(id) {
+    students = students.filter(student => student.ID !== id);
+
+    localStorage.setItem("students", JSON.stringify(students));
+
+    displayStudents();
 }
